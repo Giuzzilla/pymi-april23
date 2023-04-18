b2c6c2e1b5c11117198bc2e220a7b86ccac274ba
Giulio Mazzanti <gmazzanti@protonmail.com>
2023-04-18 19:24:51 +0200

    style: black applied to flask example

--- backend/flask-sqlalchemy1/flask-example.py
+++ backend/flask-sqlalchemy1/flask-example.py
@@ -3,15 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-items_storage = {
-    1: {"name": "Foo", "price": 42.0},
-    2: {"name": "Bar", "price": 23.0}
-}
+items_storage = {1: {"name": "Foo", "price": 42.0}, 2: {"name": "Bar", "price": 23.0}}
+
 
 @app.route("/items/")
 def read_items():
     return list(items_storage.values())
 
-@app.route("/items/item_id") # <int:item_id>
+
+@app.route("/items/item_id")  # <int:item_id>
 def read_item(item_id):
     return items_storage[item_id]
